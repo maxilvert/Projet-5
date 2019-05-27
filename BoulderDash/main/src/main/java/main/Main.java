0c4eb71c845716eda7bfd433abfbc404b7d8d259
theo <theo.aracil@viacesi.fr>
2019-05-27 09:54:52 +0200

    DEFAULT

--- BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash/main/src/main/java/main/Main.java
@@ -26,6 +26,6 @@ public abstract class Main {
         view.setController(controller);
 
         controller.control();
-        controller.orderPerform(ControllerOrder.UP);
+        controller.orderPerform(ControllerOrder.DEFAULT);
     }
 }
